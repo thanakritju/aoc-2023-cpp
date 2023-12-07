5722e7f915aede4588775bf2e8441c1e5b154514
thanakritju <thanakrit.ju.work@gmail.com>
2023-12-07 09:17:25 +0700

    save

--- day01/main.cc
+++ day01/main.cc
@@ -30,26 +30,36 @@ int countV2(std::string data)
 {
   std::map<std::string, int> map = {{"1", 1}, {"2", 2}, {"3", 3}, {"4", 4}, {"5", 5}, {"6", 6}, {"7", 7}, {"8", 8}, {"9", 9}, {"one", 1}, {"two", 2}, {"three", 3}, {"four", 4}, {"five", 5}, {"six", 6}, {"seven", 7}, {"eight", 8}, {"nine", 9}};
 
-  std::vector<int> locations;
-
-  int firstIndex = INT_MAX;
-  int lastIndex = 0;
-  int firstValue = 0;
-  int lastValue = 0;
+  size_t firstIndex = INT_MAX;
+  size_t lastIndex = 0;
+  size_t firstValue = 0;
+  size_t lastValue = 0;
   for (auto const &imap : map)
   {
     std::size_t found = data.find(imap.first);
-    if (found != std::string::npos)
+    while (true)
     {
-      if (found <= firstIndex)
+      if (found < firstIndex)
       {
         firstIndex = found;
         firstValue = imap.second;
       }
-      if (found >= lastIndex)
+      if (found > lastIndex && found < data.length())
       {
         lastIndex = found;
         lastValue = imap.second;
+      }
+      std::size_t found = data.find(imap.first, found + 1);
+      std::cout << "key " << imap.first << "\n";
+      std::cout << "found " << found << "\n";
+      std::cout << "firstValue " << firstValue << "\n";
+      std::cout << "lastValue " << lastValue << "\n";
+      std::cout << "firstIndex " << firstIndex << "\n";
+      std::cout << "lastIndex " << lastIndex << "\n";
+      std::cout << "data.length() " << data.length() << "\n";
+      if (found == std::string::npos)
+      {
+        break;
       }
     }
   }
@@ -59,20 +69,25 @@ int countV2(std::string data)
 
 int test()
 {
-  assert(count("pqr3stu8vwx") == 38);
-  std::cout << "Execution continues past the 1 assert\n";
-  assert(count("treb7uchet") == 77);
-  std::cout << "Execution continues past the 2 assert\n";
-  assert(countV2("7pqrstsixteen") == 76);
-  std::cout << "Execution continues past the 3 assert\n";
-  assert(countV2("eightwothree") == 83);
-  std::cout << "Execution continues past the 4 assert\n";
-  assert(countV2("two1nine") == 29);
-  std::cout << "Execution continues past the 5 assert\n";
-  assert(countV2("one") == 11);
-  std::cout << "Execution continues past the 5 assert\n";
-
   std::cout << "Got " << countV2("eightnine2eightnineeight") << "\n";
+
+  // assert(count("pqr3stu8vwx") == 38);
+  // std::cout << "Execution continues past the 1 assert\n";
+  // assert(count("treb7uchet") == 77);
+  // std::cout << "Execution continues past the 2 assert\n";
+  // assert(countV2("7pqrstsixteen") == 76);
+  // std::cout << "Execution continues past the 3 assert\n";
+  // assert(countV2("eightwothree") == 83);
+  // std::cout << "Execution continues past the 4 assert\n";
+  // assert(countV2("two1nine") == 29);
+  // std::cout << "Execution continues past the 5 assert\n";
+  // assert(countV2("eightnine2eightnineeight") == 88);
+  // std::cout << "Execution continues past the 6 assert\n";
+  // assert(countV2("threeghtcbbllbpnxqxljhnztsx37eight2") == 32);
+  // std::cout << "Execution continues past the 7 assert\n";
+  // assert(countV2("tg112qhrmt") == 12);
+  // std::cout << "Execution continues past the 8 assert\n";
+
   return 0;
 }
 
