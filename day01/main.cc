0b69e758da3967226cf2a5b5d5015f47c0f379ab
thanakritju <thanakrit.ju.work@gmail.com>
2023-12-07 20:03:46 +0700

    finish day1.2

--- day01/main.cc
+++ day01/main.cc
@@ -5,8 +5,9 @@
 #include <string_view>
 #include <fstream>
 #include <ranges>
-#include <ctype.h>
 #include <map>
+
+#define VERBOSE
 
 int count(std::string data)
 {
@@ -26,8 +27,50 @@ int count(std::string data)
   return first * 10 + last;
 }
 
+int countV1(std::string data)
+{
+  std::map<std::string, int> map = {{"1", 1}, {"2", 2}, {"3", 3}, {"4", 4}, {"5", 5}, {"6", 6}, {"7", 7}, {"8", 8}, {"9", 9}};
+
+  size_t firstIndex = INT_MAX;
+  size_t lastIndex = 0;
+  size_t firstValue = 0;
+  size_t lastValue = 0;
+  for (auto const &imap : map)
+  {
+    std::size_t found = data.find(imap.first);
+    do
+    {
+      if (found <= static_cast<size_t>(firstIndex))
+      {
+        firstIndex = static_cast<int>(found);
+        firstValue = imap.second;
+      }
+      if (found >= static_cast<size_t>(lastIndex) && found < data.length())
+      {
+        lastIndex = static_cast<int>(found);
+        lastValue = imap.second;
+      }
+
+      found = data.find(imap.first, found + 1);
+#ifdef VERBOSE
+      std::cout << "key " << imap.first << "\n";
+      std::cout << "found " << found << "\n";
+      std::cout << "firstValue " << firstValue << "\n";
+      std::cout << "lastValue " << lastValue << "\n";
+      std::cout << "firstIndex " << firstIndex << "\n";
+      std::cout << "lastIndex " << lastIndex << "\n";
+      std::cout << "data.length() " << data.length() << "\n";
+#endif
+    } while (found != std::string::npos);
+  }
+
+  return firstValue * 10 + lastValue;
+}
+
 int countV2(std::string data)
 {
+
+  std::cout << "Input: " << data;
   std::map<std::string, int> map = {{"1", 1}, {"2", 2}, {"3", 3}, {"4", 4}, {"5", 5}, {"6", 6}, {"7", 7}, {"8", 8}, {"9", 9}, {"one", 1}, {"two", 2}, {"three", 3}, {"four", 4}, {"five", 5}, {"six", 6}, {"seven", 7}, {"eight", 8}, {"nine", 9}};
 
   size_t firstIndex = INT_MAX;
@@ -37,19 +80,9 @@ int countV2(std::string data)
   for (auto const &imap : map)
   {
     std::size_t found = data.find(imap.first);
-    while (true)
+    do
     {
-      if (found < firstIndex)
-      {
-        firstIndex = found;
-        firstValue = imap.second;
-      }
-      if (found > lastIndex && found < data.length())
-      {
-        lastIndex = found;
-        lastValue = imap.second;
-      }
-      std::size_t found = data.find(imap.first, found + 1);
+#ifdef VERBOSE
       std::cout << "key " << imap.first << "\n";
       std::cout << "found " << found << "\n";
       std::cout << "firstValue " << firstValue << "\n";
@@ -57,36 +90,51 @@ int countV2(std::string data)
       std::cout << "firstIndex " << firstIndex << "\n";
       std::cout << "lastIndex " << lastIndex << "\n";
       std::cout << "data.length() " << data.length() << "\n";
-      if (found == std::string::npos)
+#endif
+      if (found <= static_cast<size_t>(firstIndex))
       {
-        break;
+        firstIndex = static_cast<int>(found);
+        firstValue = imap.second;
       }
-    }
+      if (found >= static_cast<size_t>(lastIndex) && found <= data.length())
+      {
+        lastIndex = static_cast<int>(found);
+        lastValue = imap.second;
+      }
+
+      found = data.find(imap.first, found + 1);
+
+    } while (found != std::string::npos);
   }
 
+  std::cout << " Output: " << firstValue * 10 + lastValue << "\n";
   return firstValue * 10 + lastValue;
 }
 
 int test()
 {
-  std::cout << "Got " << countV2("eightnine2eightnineeight") << "\n";
+  std::cout << "Got " << countV2("4") << "\n";
 
-  // assert(count("pqr3stu8vwx") == 38);
-  // std::cout << "Execution continues past the 1 assert\n";
-  // assert(count("treb7uchet") == 77);
-  // std::cout << "Execution continues past the 2 assert\n";
-  // assert(countV2("7pqrstsixteen") == 76);
-  // std::cout << "Execution continues past the 3 assert\n";
-  // assert(countV2("eightwothree") == 83);
-  // std::cout << "Execution continues past the 4 assert\n";
-  // assert(countV2("two1nine") == 29);
-  // std::cout << "Execution continues past the 5 assert\n";
-  // assert(countV2("eightnine2eightnineeight") == 88);
-  // std::cout << "Execution continues past the 6 assert\n";
-  // assert(countV2("threeghtcbbllbpnxqxljhnztsx37eight2") == 32);
-  // std::cout << "Execution continues past the 7 assert\n";
-  // assert(countV2("tg112qhrmt") == 12);
-  // std::cout << "Execution continues past the 8 assert\n";
+  assert(count("pqr3stu8vwx") == 38);
+  std::cout << "Execution continues past the 1 assert\n";
+  assert(count("treb7uchet") == 77);
+  std::cout << "Execution continues past the 2 assert\n";
+  assert(countV2("7pqrstsixteen") == 76);
+  std::cout << "Execution continues past the 3 assert\n";
+  assert(countV2("eightwothree") == 83);
+  std::cout << "Execution continues past the 4 assert\n";
+  assert(countV2("two1nine") == 29);
+  std::cout << "Execution continues past the 5 assert\n";
+  assert(countV2("eightnine2eightnineeight") == 88);
+  std::cout << "Execution continues past the 6 assert\n";
+  assert(countV2("threeghtcbbllbpnxqxljhnztsx37eight2") == 32);
+  std::cout << "Execution continues past the 7 assert\n";
+  assert(countV2("tg112qhrmt") == 12);
+  std::cout << "Execution continues past the 8 assert\n";
+  assert(countV2("eightnine2eightnineeight") == 88);
+  std::cout << "Execution continues past the 9 assert\n";
+  assert(countV2("4r") == 44);
+  std::cout << "Execution continues past the 10 assert\n";
 
   return 0;
 }
@@ -103,13 +151,16 @@ int parse_and_run(std::string_view path)
 
   std::string line;
   int sum = 0;
+  int sum1 = 0;
   int sum2 = 0;
   while (std::getline(file, line))
   {
     sum += count(line);
+    sum1 += countV1(line);
     sum2 += countV2(line);
   }
-  std::cout << "The part 1 answer is " << sum << "\n";
+  std::cout << "The part 1.1 answer is " << sum << "\n";
+  std::cout << "The part 1.2 answer is " << sum1 << "\n";
   std::cout << "The part 2 answer is " << sum2 << "\n";
 
   return 0;
--- makefile
+++ makefile
@@ -2,8 +2,15 @@ current_dir = $(shell pwd)
 
 # make day=00 run
 run:
-	BAZEL_CXXOPTS="-std=c++17" bazel run //day$(day):main $(current_dir)/day$(day)/input.txt
+	bazel run //day$(day):main $(current_dir)/day$(day)/input.txt
+
+# make day=01 example
+example:
+	bazel run //day$(day):main $(current_dir)/day$(day)/example.txt
 
 # make day=00 test
 test:
-	BAZEL_CXXOPTS="-std=c++17" bazel run //day$(day):main
+	bazel run //day$(day):main
+
+install:
+	brew install bazel
