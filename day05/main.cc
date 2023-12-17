920c2b6dbcff06ba1a6caa5a6d844388e2d49f06
thanakritju <thanakrit.ju.work@gmail.com>
2023-12-17 22:39:14 +0700

    day5 part 1

--- day05/main.cc
+++ day05/main.cc
@@ -9,12 +9,84 @@
 
 #define VERBOSE
 
-int solve(std::vector<std::string> input)
+using namespace std;
+
+struct Rule
 {
-  return 0;
+  long lower;
+  long upper;
+  long diff;
+};
+
+array<vector<Rule>, 7> init_rules(vector<string> input)
+{
+  array<vector<Rule>, 7> arr;
+  int i = -1;
+  for (const auto line : input)
+  {
+    if (line.empty())
+    {
+      i++;
+    }
+    if (isdigit(line[0]))
+    {
+      Rule r;
+      string x, y, z;
+      stringstream lineStream(line);
+      lineStream >> x;
+      lineStream >> y;
+      lineStream >> z;
+      r.lower = stol(y);
+      r.upper = stol(y) + stol(z);
+      r.diff = stol(x) - stol(y);
+
+      cout << "x: " << x;
+      cout << " y: " << y;
+      cout << " z: " << z;
+      cout << " r.lower: " << r.lower;
+      cout << " r.upper: " << r.upper;
+      cout << " r.diff: " << r.diff;
+      arr[i].push_back(r);
+    }
+    cout << endl;
+  }
+  return arr;
 }
 
-int solve2(std::vector<std::string> input)
+int solve(vector<string> input)
+{
+  string seeds = input[0];
+  stringstream seedsStream(seeds);
+  string seed;
+  seedsStream >> seed;
+  long min = LONG_MAX;
+  array<vector<Rule>, 7> arr = init_rules(input);
+  while (seedsStream >> seed)
+  {
+    long seedValue = stol(seed);
+    for (int i = 0; i < 7; i++)
+    {
+      cout << "i: " << i;
+      cout << " Value: " << seedValue << endl;
+      for (Rule r : arr[i])
+      {
+        if (r.upper >= seedValue && seedValue >= r.lower)
+        {
+          seedValue += r.diff;
+          break;
+        }
+      }
+    }
+    if (seedValue < min)
+    {
+      min = seedValue;
+    }
+    cout << endl;
+  }
+  return min;
+}
+
+int solve2(vector<string> input)
 {
   return 0;
 }
@@ -24,24 +96,24 @@ int test()
   return 0;
 }
 
-int parse_and_run(std::string_view path)
+int parse_and_run(string_view path)
 {
-  std::vector<std::string> data;
-  std::fstream file(path);
+  vector<string> data;
+  fstream file(path);
   if (!file.is_open())
   {
-    std::cerr << "Failed to open " << std::quoted(path) << "\n";
+    cerr << "Failed to open " << quoted(path) << "\n";
     return 1;
   }
 
-  std::vector<std::string> lines;
-  std::string line;
-  while (std::getline(file, line))
+  vector<string> lines;
+  string line;
+  while (getline(file, line))
   {
     lines.push_back(line);
   }
-  std::cout << "The part 1 answer is " << solve(lines) << "\n";
-  std::cout << "The part 2 answer is " << solve2(lines) << "\n";
+  cout << "The part 1 answer is " << solve(lines) << "\n";
+  cout << "The part 2 answer is " << solve2(lines) << "\n";
 
   return 0;
 }
--- template/main.cc
+++ template/main.cc
@@ -9,12 +9,14 @@
 
 #define VERBOSE
 
-int solve(std::vector<std::string> input)
+using namespace std;
+
+int solve(vector<string> input)
 {
   return 0;
 }
 
-int solve2(std::vector<std::string> input)
+int solve2(vector<string> input)
 {
   return 0;
 }
@@ -24,24 +26,24 @@ int test()
   return 0;
 }
 
-int parse_and_run(std::string_view path)
+int parse_and_run(string_view path)
 {
-  std::vector<std::string> data;
-  std::fstream file(path);
+  vector<string> data;
+  fstream file(path);
   if (!file.is_open())
   {
-    std::cerr << "Failed to open " << std::quoted(path) << "\n";
+    cerr << "Failed to open " << quoted(path) << "\n";
     return 1;
   }
 
-  std::vector<std::string> lines;
-  std::string line;
-  while (std::getline(file, line))
+  vector<string> lines;
+  string line;
+  while (getline(file, line))
   {
     lines.push_back(line);
   }
-  std::cout << "The part 1 answer is " << solve(lines) << "\n";
-  std::cout << "The part 2 answer is " << solve2(lines) << "\n";
+  cout << "The part 1 answer is " << solve(lines) << "\n";
+  cout << "The part 2 answer is " << solve2(lines) << "\n";
 
   return 0;
 }
