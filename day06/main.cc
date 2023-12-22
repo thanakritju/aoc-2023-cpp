f6915320724d315cb46021199f28caff805fb845
thanakritju <thanakrit.ju.work@gmail.com>
2023-12-22 12:09:21 +0700

    day6

--- day06/main.cc
+++ day06/main.cc
@@ -11,18 +11,65 @@
 
 using namespace std;
 
+long possible_way_to_win(long time, long distance)
+{
+  long count = 0;
+  for (long i = 0; i < time; i++)
+  {
+    if (i * (time - i) > distance)
+    {
+      count++;
+    };
+  }
+  return count;
+}
+
 int solve(vector<string> input)
 {
-  return 0;
+  std::stringstream times(input[0]);
+  string time;
+  times >> time;
+  std::stringstream distances(input[1]);
+  string distance;
+  distances >> distance;
+  long ans = 1;
+
+  while (times >> time)
+  {
+    distances >> distance;
+    cout << "Time: " << time << " Distance: " << distance << endl;
+    ans *= possible_way_to_win(stol(time), stol(distance));
+  }
+
+  return ans;
 }
 
 int solve2(vector<string> input)
 {
-  return 0;
+  std::stringstream times(input[0]);
+  string timeToken;
+  string time = "";
+  times >> timeToken;
+  std::stringstream distances(input[1]);
+  string distanceToken;
+  string distance = "";
+  distances >> distanceToken;
+
+  while (times >> timeToken)
+  {
+    distances >> distanceToken;
+    time += timeToken;
+    distance += distanceToken;
+  }
+
+  return possible_way_to_win(stol(time), stol(distance));
 }
 
 int test()
 {
+  assert(possible_way_to_win(7, 9) == 4);
+  assert(possible_way_to_win(15, 40) == 8);
+  assert(possible_way_to_win(30, 200) == 9);
   return 0;
 }
 
