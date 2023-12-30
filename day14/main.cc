cbe99a2c130e624c85b7da25b11de12d2b7ea0d0
thanakritju <thanakrit.ju.work@gmail.com>
2023-12-30 12:44:10 +0700

    day14 part1

--- day14/main.cc
+++ day14/main.cc
@@ -6,14 +6,103 @@
 #include <fstream>
 #include <ranges>
 #include <sstream>
+#include <set>
 
 #define VERBOSE
 
 using namespace std;
 
+string sort(string s)
+{
+  string ns = "";
+  int countO = 0;
+  int count = 0;
+  for (char c : s)
+  {
+    if (c == 'O')
+    {
+      countO++;
+    }
+    if (c == '#')
+    {
+      ns += string(countO, 'O');
+      ns += string(count, '.');
+      ns += '#';
+      count = 0;
+      countO = 0;
+    }
+    if (c == '.')
+    {
+      count++;
+    }
+  }
+  ns += string(countO, 'O');
+  ns += string(count, '.');
+  return ns;
+}
+
+vector<string> sort_all(vector<string> input)
+{
+  vector<string> vs;
+  for (string s : input)
+  {
+    vs.push_back(sort(s));
+  }
+  return vs;
+}
+
+vector<string> rotate_clockwise(vector<string> input)
+{
+  vector<string> vs;
+  for (string _ : input)
+  {
+    vs.push_back("");
+  }
+
+  for (string s : input)
+  {
+    int row = 0;
+    for (char c : s)
+    {
+      vs[row] = c + vs[row];
+      row++;
+    }
+  }
+
+  return vs;
+}
+
+int count_load(vector<string> input)
+{
+  int n = input.size();
+  int load = 0;
+  for (string s : input)
+  {
+    int j = n;
+    for (char c : s)
+    {
+      if (c == 'O')
+      {
+        load += j;
+      }
+      j--;
+    }
+  }
+  return load;
+}
+
+void print(vector<string> input)
+{
+  for (string s : input)
+  {
+    cout << s << endl;
+  }
+  cout << endl;
+}
+
 long solve(vector<string> input)
 {
-  return 0;
+  return count_load(sort_all(rotate_clockwise(rotate_clockwise(rotate_clockwise(input)))));
 }
 
 long solve2(vector<string> input)
@@ -23,6 +112,12 @@ long solve2(vector<string> input)
 
 int test()
 {
+  cout << sort("O.OO#....#") << endl;
+  assert(sort("O.") == "O.");
+  assert(sort(".O") == "O.");
+  assert(sort("O.OO#....#") == "OOO.#....#");
+  assert(sort("O.OO#..O.#") == "OOO.#O...#");
+  assert(sort("O.OO#..O.#.O") == "OOO.#O...#O.");
   return 0;
 }
 
