f11f9e181c8e211b998ab460b2f047076fbf6abb
thanakritju <thanakrit.ju.work@gmail.com>
2023-12-30 16:30:16 +0700

    fix windows generate

---
+++ day15/main.cc
@@ -0,0 +1,64 @@
+#include <vector>
+#include <string>
+#include <iostream>
+#include <cassert>
+#include <fstream>
+#include <ranges>
+#include <sstream>
+
+#define VERBOSE
+
+using namespace std;
+
+long solve(vector<string> input)
+{
+  return 0;
+}
+
+long solve2(vector<string> input)
+{
+  return 0;
+}
+
+int test()
+{
+  return 0;
+}
+
+int parse_and_run(string path)
+{
+  vector<string> data;
+  ifstream file(path);
+  if (!file.is_open())
+  {
+    cerr << "Failed to open " << path << endl;
+    return 1;
+  }
+
+  vector<string> lines;
+  string line;
+  while (getline(file, line))
+  {
+    lines.push_back(line);
+  }
+  long ans1 = solve(lines);
+  long ans2 = solve2(lines);
+  cout << "--------------------------" << endl;
+  cout << "The part 1 answer is " << ans1 << endl;
+  cout << "The part 2 answer is " << ans2 << endl;
+  cout << "--------------------------" << endl;
+
+  return 0;
+}
+
+int main(int argc, char **argv)
+{
+  if (argc == 1)
+  {
+    return test();
+  }
+  else if (argc == 2)
+  {
+    return parse_and_run(argv[1]);
+  }
+}
